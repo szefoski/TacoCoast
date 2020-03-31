b4a0d7f47e52235af7e7ba437c88d5ca418ffb27
szefoski <szefoski22@gmail.com>
2020-04-01 00:22:02 +0200

    basic spring app

---
+++ src/main/java/com/szefoski/tacocoast/TacoCoastApplication.java
@@ -0,0 +1,13 @@
+package com.szefoski.tacocoast;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TacoCoastApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TacoCoastApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/szefoski/tacocoast/TacoCoastApplicationTests.java
@@ -0,0 +1,13 @@
+package com.szefoski.tacocoast;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TacoCoastApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
