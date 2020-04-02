2faf0be87da4587f933c5d684803accce56707a8
szefoski <szefoski22@gmail.com>
2020-04-02 13:58:04 +0200

    added home page

---
+++ src/main/java/com/szefoski/tacoscoast/HomeController.java
@@ -0,0 +1,13 @@
+package com.szefoski.tacoscoast;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+
+    @GetMapping("/")
+    public String home() {
+        return "home";
+    }
+}
--- src/test/java/com/szefoski/tacoscoast/TacosCoastApplicationTests.java
+++ src/test/java/com/szefoski/tacoscoast/TacosCoastApplicationTests.java
@@ -1,12 +1,28 @@
 package com.szefoski.tacoscoast;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.hamcrest.Matchers.containsString;
 
 @SpringBootTest
+@AutoConfigureMockMvc
 class TacosCoastApplicationTests {
 
+    @Autowired
+    private MockMvc mockMvc;
+
     @Test
-    void contextLoads() {
+    void contextLoads() throws Exception{
+       mockMvc.perform(get("/"))
+               .andExpect(status().isOk())
+               .andExpect(view().name("home"))
+               .andExpect(content().string(containsString("Welcome to...")));
     }
 }
